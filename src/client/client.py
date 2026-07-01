da4133d0116b7e87451761322f904b87bfbfc498
Bigfootpp <menzer.yakin2010@gmail.com>
2026-07-01 19:22:02 +0200

    feat(client): update Client class to use websocket connection and add connect/close methods

--- src/client/client.py
+++ src/client/client.py
@@ -1,10 +1,17 @@
-# import websockets
+import websockets
 
 class Client:
-    def __init__(self, url: str = "ws://127.0.0.1:8000/ws"):
-        self.url = url
-        self.connection = None
-        self.connected = False
+    def __init__(self, uri: str = "ws://127.0.0.1:8000/ws"):
+        self.uri = uri
+        self.websocket = None
     
     async def connect(self):
+        self.websocket = await websockets.connect(self.uri)
+    
+    async def process_input(self, input: str):
         pass
+
+    async def close(self):
+        if self.websocket:
+            await self.websocket.close()
+            self.websocket = None
