11ae887ed7f19256da171019d264925ab2a2a496
Bigfootpp <menzer.yakin2010@gmail.com>
2026-07-02 00:21:13 +0200

    feat(dispatcher): add status event

--- src/agent_core/streaming.py
+++ src/agent_core/streaming.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
 from dataclasses import dataclass, asdict
-from typing import AsyncIterator
+from typing import AsyncIterator, Literal
 
 @dataclass
 class Event:
@@ -14,6 +14,10 @@ class Event:
     def __str__(self) -> str:
         return json.dumps(self.to_dict())
 
+class StatusEvent(Event):
+    def __init__(self, content: Literal["idle", "responding", "thinking"]):
+        super().__init__(type="status", data={"state": content})
+
 class ResponseChunkEvent(Event):
     def __init__(self, content: str):
         super().__init__(type="response", data={"chunk": content})
@@ -22,6 +26,7 @@ class ThoughtChunkEvent(Event):
     def __init__(self, content: str):
         super().__init__(type="thought", data={"chunk": content})
 
+# Mock stream class
 class Stream:
     async def __aiter__(self) -> AsyncIterator[Event]:
         await asyncio.sleep(1)
--- src/gateway/dispatcher.py
+++ src/gateway/dispatcher.py
@@ -1,7 +1,16 @@
 from typing import AsyncIterator
 
 from agent_core.harness import Harness
-from agent_core.streaming import Event
+from agent_core.streaming import Event, StatusEvent, ResponseChunkEvent, ThoughtChunkEvent
+
+idle_status = StatusEvent("idle")
+responding_status = StatusEvent("responding")
+thinking_status = StatusEvent("thinking")
+
+EVENT_STATUS_MAPPING: dict[type[Event], StatusEvent] = {
+    ResponseChunkEvent: responding_status,
+    ThoughtChunkEvent: thinking_status
+}
 
 class Dispatcher:
     def __init__(self):
@@ -10,5 +19,11 @@ class Dispatcher:
     async def dispatch(self, message: Event) -> AsyncIterator[Event]:
         match message.type:
             case "send_message":
+                status = idle_status
                 async for event in await self.harness.process_input(message.data["text"]):
+                    current_status = EVENT_STATUS_MAPPING[event.__class__]
+                    if current_status != status:
+                        status = current_status
+                        yield current_status
                     yield event
+                yield idle_status
