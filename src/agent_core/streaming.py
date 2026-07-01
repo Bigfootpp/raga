db96c5a29294e1671dd115e294c05079708a66e3
Bigfootpp <menzer.yakin2010@gmail.com>
2026-07-01 23:09:49 +0200

    feat(streaming): add delays between yielding ResponseChunkEvent instances

--- src/agent_core/streaming.py
+++ src/agent_core/streaming.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from dataclasses import dataclass, asdict
 from typing import AsyncIterator
@@ -23,7 +24,11 @@ class ThoughtChunkEvent(Event):
 
 class Stream:
     async def __aiter__(self) -> AsyncIterator[Event]:
+        await asyncio.sleep(1)
         yield ResponseChunkEvent("This")
+        await asyncio.sleep(1)
         yield ResponseChunkEvent(" is")
+        await asyncio.sleep(1)
         yield ResponseChunkEvent(" a")
+        await asyncio.sleep(1)
         yield ResponseChunkEvent(" test")
