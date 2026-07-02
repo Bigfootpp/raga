d51bd9d568e36cbf2aafe9e9bd5ef10beb4e3089
Bigfootpp <menzer.yakin2010@gmail.com>
2026-07-02 22:28:01 +0200

    refactor(events): refactor Message class to use generics for type safety

--- src/utils/events.py
+++ src/utils/events.py
@@ -1,20 +1,30 @@
 import json
 from dataclasses import dataclass, asdict
-from typing import Literal
+from typing import Any, Literal
 
 STATUS_TYPE = Literal["idle", "responding", "thinking"]
+
 EVENT_TYPE = Literal["response", "thought", "status"]
+ACTION_TYPE = Literal["send_message", "interrupt"]
+
+MESSAGE_TYPE = Literal[EVENT_TYPE, ACTION_TYPE]
 
 @dataclass
-class Event:
-    type: str
-    data: dict
+class Message:
+    type: MESSAGE_TYPE
+    data: dict[str, Any]
 
     def to_dict(self):
         return asdict(self)
     
     def __str__(self) -> str:
         return json.dumps(self.to_dict())
+
+class Event(Message):
+    pass
+
+class Action(Message):
+    pass
 
 # SERVER
 class StatusEvent(Event):
@@ -28,3 +38,8 @@ class ResponseChunkEvent(Event):
 class ThoughtChunkEvent(Event):
     def __init__(self, content: str):
         super().__init__(type="thought", data={"chunk": content})
+
+# CLIENT
+class SendMessageAction(Action):
+    def __init__(self, content: str) -> None:
+        super().__init__(type="send_message", data={"text": content})
