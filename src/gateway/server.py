9b58044a6a5edd07395ae57c829b73822c80bc7e
Bigfootpp <menzer.yakin2010@gmail.com>
2026-07-07 02:23:39 +0200

    feat(server): enhance error handling in websocket endpoint

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from fastapi import FastAPI, WebSocket, WebSocketException
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, WebSocketException
 
 from gateway.dispatcher import Dispatcher
 from pydantic import ValidationError
@@ -18,29 +18,34 @@ async def websocket_endpoint(websocket: WebSocket):
 
     dispatcher = Dispatcher()
 
-    async def handle_error(e: Exception):
-        print(e.__class__)
-        print(e)
+    async def handle_error(e: Exception) -> bool:
+        print(f"Error: ({e.__class__.__name__}): {e}")
         match e:
             case ValidationError():
-                await websocket.send_json({
-                    "type": "error",
-                    "data": {
-                        "message": "Invalid format",
-                        "details": e.errors()
-                    }
-                })
-                return
-            case WebSocketException():
-                return
+                try:
+                    await websocket.send_json({
+                        "type": "error",
+                        "data": {
+                            "message": "Invalid format",
+                            "details": e.errors()
+                        }
+                    })
+                except Exception:
+                    return True
+                return False
+            case WebSocketException() | WebSocketDisconnect():
+                return True
             case Exception():
-                await websocket.send_json({
-                    "type": "error",
-                    "data": {
-                        "message": "Une erreur interne est survenue durant le traitement."
-                    }
-                })
-                return
+                try:
+                    await websocket.send_json({
+                        "type": "error",
+                        "data": {
+                            "message": "An internal error occurred during processing."
+                        }
+                    })
+                except Exception:
+                    return True
+                return False
 
     async def handle(action_json: str):
         try:
@@ -58,4 +63,11 @@ async def websocket_endpoint(websocket: WebSocket):
             action: str = await websocket.receive_text()
             asyncio.create_task(handle(action_json=action))
         except Exception as e:
-            await handle_error(e=e)
+            need_break = await handle_error(e=e)
+            if need_break:
+                break
+    
+    try:
+        await websocket.close()
+    except Exception:
+        pass
