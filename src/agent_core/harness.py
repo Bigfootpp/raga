1a2fa25575e49a09ec0baeff41fedeecf165b76b
Bigfootpp <menzer.yakin2010@gmail.com>
2026-07-08 18:17:21 +0200

    feat(harness): implement interrupt

--- src/agent_core/harness.py
+++ src/agent_core/harness.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import AsyncIterator
+from typing import AsyncIterator, Optional
 
 from agent_core.agent import Agent
 from agent_core.session import Session, UserContent
@@ -8,17 +8,34 @@ from utils.frames import Event
 class AgentBusyError(Exception):
     pass
 
+class AgentNotBusyError(Exception):
+    pass
+
 class Harness:
     def __init__(self):
         self.session = Session([])
         self.agent = Agent(self.session)
         self.lock = asyncio.Lock()
+        self._current_task: Optional[asyncio.Task] = None
     
+    async def interrupt(self):
+        if self._current_task and not self._current_task.done():
+            self._current_task.cancel()
+
     async def process_input(self, input: str) -> AsyncIterator[Event]:
-        if not self.lock.locked():
-            async with self.lock:
-                self.session.add_message(UserContent(input))
+        if self.lock.locked():
+            raise AgentBusyError("Agent is busy")
+
+        async with self.lock:
+            self._current_task = asyncio.current_task()
+
+            self.session.save_state()
+            self.session.add_message(UserContent(input))
+            try:
                 async for event in await self.agent.run():
                     yield event
-        else:
-            raise AgentBusyError("Agent is busy")
+            except asyncio.CancelledError:
+                self.session.load_state()
+                raise
+            finally:
+                self._current_task = None
--- src/agent_core/session.py
+++ src/agent_core/session.py
@@ -14,7 +14,14 @@ class AgentContent(Message):
 class Session:
     def __init__(self, history: list[Message]):
         self.history = history
+        self.state = []
     
+    def save_state(self):
+        self.state = self.history.copy()
+    
+    def load_state(self):
+        self.history = self.state.copy()
+
     def load_history(self, history: list[Message]):
         self.history = history
     
